b63968be97a808fe9834ca911b6739c66bdf0ee8
Frumosi <kkone.devops@gmail.com>
2024-11-09 09:47:04 -0500

    Adding temporary changes to Github

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { registerUser, loginUser } = require('../controllers/userController');
+const userController = require('../controllers/userController');
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-// Add more routes for getting user, updating user, etc.
+router.put('/:username', userController.updateUserProfile);
 
 module.exports = router;
